07bf722749e36f26075e8c9490d88e7890685785
Paul Sarando <psarando@iplantcollaborative.org>
2016-09-07 16:01:21 -0700

    CORE-8177 Update compojure-api version to 1.1.8
    Update to cheshire 5.6.3 (compojure-api dependency).

--- project.clj
+++ project.clj
@@ -6,13 +6,13 @@
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cheshire "5.5.0"
+                 [cheshire "5.6.3"
                   :exclusions [[com.fasterxml.jackson.dataformat/jackson-dataformat-cbor]
                                [com.fasterxml.jackson.dataformat/jackson-dataformat-smile]
                                [com.fasterxml.jackson.core/jackson-annotations]
                                [com.fasterxml.jackson.core/jackson-databind]
                                [com.fasterxml.jackson.core/jackson-core]]]
                  [clj-time "0.9.0"] ; required due to bug in lein-ring
-                 [metosin/compojure-api "0.24.5"]]
+                 [metosin/compojure-api "1.1.8"]]
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
                    :plugins [[lein-ring "0.9.6"]]}})
