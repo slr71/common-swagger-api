04037d36c52c170e93838ebc6426df910ecda83d
Ian McEwen <mian@cyverse.org>
2016-09-26 10:51:31 -0700

    Version 2.8.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/common-swagger-api "2.8.1-SNAPSHOT"
+(defproject org.cyverse/common-swagger-api "2.8.1"
   :description "Common library for Swagger documented RESTful APIs"
   :url "https://github.com/cyverse-de/common-swagger-api"
   :license {:name "BSD"
