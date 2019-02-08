e2b8cb898e415ba45aa51a9f9c4005aab2cb4b0a
Sarah Roberts <sarahr@cyverse.org>
2019-02-08 16:44:26 -0700

    CORE-4: removed periods from schema element descriptions

--- src/common_swagger_api/schema/groups.clj
+++ src/common_swagger_api/schema/groups.clj
@@ -114,18 +114,18 @@
   {:results (describe [GroupMemberSubjectUpdateResponse] "The list of membership update results")})
 
 (s/defschema GroupPrivilegeUpdate
-  {:subject_id (describe String "The subject ID.")
-   :privileges (describe [ValidGroupPrivileges] "The group privileges to assign.")})
+  {:subject_id (describe String "The subject ID")
+   :privileges (describe [ValidGroupPrivileges] "The group privileges to assign")})
 
 (s/defschema GroupPrivilegeUpdates
-  {:updates (describe [GroupPrivilegeUpdate] "The privilege updates to process.")})
+  {:updates (describe [GroupPrivilegeUpdate] "The privilege updates to process")})
 
 (s/defschema GroupPrivilegeRemoval
-  {:subject_id (describe String "The subject ID.")
-   :privileges (describe [ValidGroupPrivileges] "The group privileges to remove.")})
+  {:subject_id (describe String "The subject ID")
+   :privileges (describe [ValidGroupPrivileges] "The group privileges to remove")})
 
 (s/defschema GroupPrivilegeRemovals
-  {:updates (describe [GroupPrivilegeRemoval] "The privilege updates to process.")})
+  {:updates (describe [GroupPrivilegeRemoval] "The privilege updates to process")})
 
 (s/defschema Privilege
   {:type
