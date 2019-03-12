ceb861c5a3e6a5f38657e3296d45459dcb6c5062
Sarah Roberts <sarahr@cyverse.org>
2019-03-12 11:52:31 -0700

    bumped the snapshot release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/common-swagger-api "2.9.3"
+(defproject org.cyverse/common-swagger-api "2.9.4-SNAPSHOT"
   :description "Common library for Swagger documented RESTful APIs"
   :url "https://github.com/cyverse-de/common-swagger-api"
   :license {:name "BSD"
