86f17cc4fedc0976537f16b0916beb89aaf1284c
Paul Sarando <psarando@cyverse.org>
2019-05-13 18:19:11 -0700

    Version 2.11.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/common-swagger-api "2.11.7"
+(defproject org.cyverse/common-swagger-api "2.11.8-SNAPSHOT"
   :description "Common library for Swagger documented RESTful APIs"
   :url "https://github.com/cyverse-de/common-swagger-api"
   :license {:name "BSD"
