fb117a146738a8d3d3e4f9e957c5aa108f53d9ea
Paul Sarando <psarando@cyverse.org>
2019-06-12 18:39:11 -0700

    CORE-9 Import schemas for /navigation endpoints.
    Import schemas from data-info.routes.schemas.navigation into
    common-swagger-api.schema.data.navigation

--- src/common_swagger_api/schema/data/navigation.clj
+++ src/common_swagger_api/schema/data/navigation.clj
@@ -1,8 +1,25 @@
 (ns common-swagger-api.schema.data.navigation
-  (:use [common-swagger-api.schema :only [describe]])
+  (:use [common-swagger-api.schema :only [describe]]
+        [common-swagger-api.schema.stats :as stats-schema])
   (:require [schema.core :as s]))
 
 (s/defschema UserBasePaths
   {:user_home_path  (describe String "The absolute path to the user's home folder")
    :user_trash_path (describe String "The absolute path to the user's trash folder")
    :base_trash_path (describe String "The absolute path to the base trash folder")})
+
+(s/defschema RootListing
+  (dissoc stats-schema/DataStatInfo :type))
+
+(s/defschema NavigationRootResponse
+  {:roots      [RootListing]
+   :base-paths UserBasePaths})
+
+(s/defschema FolderListing
+  (-> stats-schema/DataStatInfo
+      (dissoc :type)
+      (assoc (s/optional-key :folders)
+             (describe [(s/recursive #'FolderListing)] "Subdirectories of this directory"))))
+
+(s/defschema NavigationResponse
+  {:folder FolderListing})
