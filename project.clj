f381146cd8b907ee9d28d467aa6e44d1f88d017d
Ashley Ramsey <aramsey@iplantcollaborative.org>
2020-06-10 12:09:40 -0700

    Version 3.0.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/common-swagger-api "3.0.2"
+(defproject org.cyverse/common-swagger-api "3.0.3-SNAPSHOT"
   :description "Common library for Swagger documented RESTful APIs"
   :url "https://github.com/cyverse-de/common-swagger-api"
   :license {:name "BSD"
