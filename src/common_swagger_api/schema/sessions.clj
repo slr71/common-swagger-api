0cede0b2a7cdbe74a56be0ff7ea85526ee899293
Sarah Roberts <sarahr@cyverse.org>
2020-07-14 12:36:39 -0700

    CORE-1094: make the ip-address parameter optional

--- src/common_swagger_api/schema/sessions.clj
+++ src/common_swagger_api/schema/sessions.clj
@@ -1,13 +1,14 @@
 (ns common-swagger-api.schema.sessions
   (:use [common-swagger-api.schema :only [describe]]
         [common-swagger-api.schema.oauth :only [RedirectUrisResponse]]
-        [schema.core :only [defschema]]))
+        [schema.core :only [defschema optional-key]]))
 
 (def LogoutSummary "Record a User Logout")
 (def LogoutDocs "This service records the fact that the user logged out.")
 
 (defschema IPAddrParam
-  {:ip-address (describe String "The IP address of the requesting user, for matching login to logout requests.")})
+  {(optional-key :ip-address)
+   (describe String "The IP address of the requesting user, for matching login to logout requests.")})
 
 (defschema LoginResponse
   {:login_time    (describe Long "Login time as milliseconds since the epoch.")
