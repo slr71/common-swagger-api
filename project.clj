2233ff4a111c714b8fa9aae04098188bfe43f75f
Sarah Roberts <sarahr@cyverse.org>
2021-05-27 17:05:59 -0700

    Version 3.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/common-swagger-api "3.1.1"
+(defproject org.cyverse/common-swagger-api "3.1.2-SNAPSHOT"
   :description "Common library for Swagger documented RESTful APIs"
   :url "https://github.com/cyverse-de/common-swagger-api"
   :license {:name "BSD"
