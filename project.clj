d9f07402da0d4ff32f4fdbdfddbc8186d5e4f9b8
Paul Sarando <psarando@cyverse.org>
2021-10-08 13:50:24 -0700

    Version 3.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/common-swagger-api "3.2.0"
+(defproject org.cyverse/common-swagger-api "3.2.1-SNAPSHOT"
   :description "Common library for Swagger documented RESTful APIs"
   :url "https://github.com/cyverse-de/common-swagger-api"
   :license {:name "BSD"
