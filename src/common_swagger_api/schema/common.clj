bd12cd75fc0e6f64929a9db18a0e047189680194
Sarah Roberts <sarahr@cyverse.org>
2021-12-01 15:16:45 -0700

    CORE-1634: added the `include-deleted` parameter to analysis and analysis stat listings

--- src/common_swagger_api/schema/common.clj
+++ src/common_swagger_api/schema/common.clj
@@ -4,4 +4,8 @@
 
 (defschema IncludeHiddenParams
   {(optional-key :include-hidden)
-   (describe Boolean "True if hidden elements should be included in the results.")})
+   (describe Boolean "True if hidden elements should be included in the results")})
+
+(defschema IncludeDeletedParams
+  {(optional-key :include-deleted)
+   (describe Boolean "True if deleted elements should be included in the results")})
