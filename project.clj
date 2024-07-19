bb927b8b64eec2be9836c6465f5eb087e0fedbee
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-19 16:42:44 -0700

    CORE-2006: updated dependencies

--- project.clj
+++ project.clj
@@ -5,17 +5,17 @@
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [cheshire "5.10.0"]
-                 [metosin/compojure-api "1.1.13"]
-                 [metosin/schema-tools "0.12.3"]
-                 [org.cyverse/clojure-commons "3.0.6"]
-                 [org.cyverse/heuristomancer "2.8.6"]
-                 [org.flatland/ordered "1.5.9"]]
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [cheshire "5.13.0"]
+                 [metosin/compojure-api "1.1.14"]
+                 [metosin/schema-tools "0.13.1"]
+                 [org.cyverse/clojure-commons "3.0.8"]
+                 [org.cyverse/heuristomancer "2.8.7"]
+                 [org.flatland/ordered "1.15.12"]]
   :eastwood {:exclude-namespaces [common-swagger-api.schema.data.exists
                                   common-swagger-api.schema.data.tickets
                                   common-swagger-api.schema.stats]
              :linters [:wrong-arity :wrong-ns-form :wrong-pre-post :wrong-tag :misplaced-docstrings]}
   :plugins [[lein-ancient "0.7.0"]
-            [test2junit "1.2.2"]
-            [jonase/eastwood "0.3.14"]])
+            [test2junit "1.4.4"]
+            [jonase/eastwood "1.4.3"]])
