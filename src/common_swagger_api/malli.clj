ed058d01de4dc5945a2f936905d1de6fdfa2d677
Sarah Roberts <sonoran.sarah@gmail.com>
2025-04-21 16:10:07 -0700

    CORE-2046: added a malli schema definition for API status responses

--- src/common_swagger_api/malli.clj
+++ src/common_swagger_api/malli.clj
@@ -3,3 +3,25 @@
             [malli.core :as m]))
 
 (def NonBlankString [:and :string [:fn (complement string/blank?)]])
+
+(def StatusResponse
+  [:map {:closed true}
+   [:service
+    {:description         "The name of the service"
+     :json-schema/example "example-api"}
+    NonBlankString]
+
+   [:description
+    {:description         "The service description"
+     :json-schema/example "An API"}
+    NonBlankString]
+
+   [:version
+    {:description         "The service version"
+     :json-schema/example "1.2.3"}
+    NonBlankString]
+
+   [:docs-url
+    {:description         "The URL for the API documentation"
+     :json-schema/example "http://example-api/docs"}
+    NonBlankString]])
